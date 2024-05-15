43f786c8a6ede4a7dcb6b07b606e7225e8385751
Laurence Leysen TM <laurenceleysen5@gmail.com>
2024-05-15 23:31:01 +0200

    Improved api / bug fixes

--- flask_api.py
+++ flask_api.py
@@ -1,41 +1,25 @@
-import time
 import json
+import asyncio
+import threading
 from flask import Flask, request, jsonify
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
 app = Flask(__name__)
-
 match_data = []
 
-class FileWatcher(FileSystemEventHandler):
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.load_data()
-        
-    def on_modified(self, event):
-        if event.src_path.endswith(self.file_path):
-            self.load_data()
-
-    def load_data(self):
-        global match_data
+async def load_data():
+    global match_data
+    while True:
         try:
-            with open(self.file_path, 'r') as f:
+            with open('../match_data.json', 'r') as f:
                 match_data = json.load(f)
-            print(f"Data reloaded from {self.file_path}")
+            print("Data reloaded from match_data.json")
         except Exception as e:
-            print(f"Failed to load data from {self.file_path}: {e}")
-
-def start_file_watcher(file_path):
-    event_handler = FileWatcher(file_path)
-    observer = Observer()
-    observer.schedule(event_handler, path='.', recursive=False)
-    observer.start()
-    return observer
+            print(f"Failed to load data from match_data.json: {e}")
+        await asyncio.sleep(5)
 
 @app.route('/api/live', methods=['GET'])
 def get_live_matches():
-    live_matches = [match for match in match_data if match.get('status') == 'Live']
+    live_matches = [match for match in match_data if match.get('status') == 'Live' or match.get('status') == 'Halftime']
     return jsonify(live_matches), 200
 
 @app.route('/api/upcoming', methods=['GET'])
@@ -62,11 +46,16 @@ def get_match_by_team():
 
     return jsonify(live_matches), 200
 
+def start_flask_app():
+    app.run(debug=True, use_reloader=False)
+
 if __name__ == '__main__':
-    json_file_path = 'match_data.json'
-    observer = start_file_watcher(json_file_path)
-    try:
-        app.run(debug=True, use_reloader=False)
-    finally:
-        observer.stop()
-        observer.join()
+    loop = asyncio.get_event_loop()
+    loop.create_task(load_data())
+
+    # Run Flask app in a separate thread
+    flask_thread = threading.Thread(target=start_flask_app)
+    flask_thread.start()
+
+    # Run the asyncio event loop
+    loop.run_forever()
